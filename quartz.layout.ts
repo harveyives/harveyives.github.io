e9f48260b760f18f4f11f58c68ff87b7dfc53de8
Harv <harveyives@users.noreply.github.com>
2025-04-19 11:46:21 +0000

    Quartz sync: Apr 19, 2025, 11:46 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,18 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: 'giscus',
+      options: {
+        repo: 'harveyives/blog',
+        repoId: 'R_kgDOOcMuBg',
+        category: 'Announcements',
+        // from data-category-id
+        categoryId: 'DIC_kwDOOcMuBs4CpQbo',
+      }
+    }),
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
