0cb323b91739b5b522a9bb190b478b4178249eda
Harv <harveyives@users.noreply.github.com>
2025-10-06 15:11:31 +0000

    disable external link icon config

--- quartz.config.ts
+++ quartz.config.ts
@@ -1,6 +1,8 @@
 import { QuartzConfig } from "./quartz/cfg"
 import * as Plugin from "./quartz/plugins"
+import { prepareLetterboxdFile } from "./quartz/plugins/emitters/letterboxd"
 
+await prepareLetterboxdFile("harveyives")
 /**
  * Quartz 4 Configuration
  *
@@ -71,10 +73,14 @@ const config: QuartzConfig = {
         },
         keepBackground: false,
       }),
+      
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
-      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.CrawlLinks({ 
+        markdownLinkResolution: "shortest",
+        externalLinkIcon: false
+      }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
     ],
@@ -94,7 +100,6 @@ const config: QuartzConfig = {
       Plugin.NotFoundPage(),
       // Comment out CustomOgImages to speed up build time
       Plugin.CustomOgImages(),
-      Plugin.Letterboxd({ username: "harveyives" }),
     ],
   },
 }
