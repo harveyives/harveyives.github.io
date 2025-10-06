1ba295fbdcd188ead49c4bb4443bbd51939f63d1
Harv <harveyives@users.noreply.github.com>
2025-10-06 13:28:53 +0000

    fix page title on mobile

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,6 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "Harvey Ives",
+    pageTitleShort: "Harv",
     pageTitleSuffix: " - Harvey Ives",
     enableSPA: true,
     enablePopovers: false,
